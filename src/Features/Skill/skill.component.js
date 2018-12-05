3fd44efbb8559ebb42bc48c5b0ad3d94b5643c8b
Ole Martin Kristiansen <olemak@apasje.no>
2018-12-05 09:20:02 +0100

    Reduce skill improvements and base to single value

--- src/Features/Skill/skill.component.js
+++ src/Features/Skill/skill.component.js
@@ -3,26 +3,23 @@ import { SkillImprovementContext } from "./skill-improvement.context";
 
 export class Skill extends React.Component {
     getSum(total, current) {
-        console.dir(total);
-        console.dir(current);
-        console.log(current + total);
-        return total + current.value;
+        return total + current;
     }
 
     render() {
         const currentSkill = this.context.skills.find(
             skill => skill.id === this.props.id
         );
+
         const currentValue = currentSkill.improvements
             .map(improvement => improvement.value)
             .reduce(this.getSum, this.props.base);
-        console.log("---------");
+
         return (
-            <div>
-                <h3>{this.props.name}</h3>
-                {<p>{currentSkill.id}</p>}
-                {<p>{currentValue}</p>}
-            </div>
+            <li>
+                {this.props.name} {currentValue} |{" "}
+                {Math.floor(currentValue / 2)} | {Math.floor(currentValue / 5)}
+            </li>
         );
     }
 }
