42ad15569ddf12bd4a7cadf8675dc660a336803c
Ole Martin Kristiansen <olemak@apasje.no>
2018-12-06 00:07:44 +0100

    Refactoring to compound pattern Static partials Render Children pattern Destructured props ClassNames

--- src/Features/Skill/skill-list.component.js
+++ src/Features/Skill/skill-list.component.js
@@ -2,25 +2,40 @@ import React from "react";
 import { SkillImprovementContext } from "./skill-improvement.context";
 import { getValue, getImprovements } from "./skill.functions";
 
-//const renderSkill = props => <Skill {...props} key={props.id} />;
-
 export class SkillList extends React.Component {
+    static displayName = "SkillList";
+    static contextType = SkillImprovementContext;
+    static Wrapper = ({ children } = {}) => <section>{children}</section>;
+    static Item = ({ name, value, className } = {}) => (
+        <article className={className}>
+            <span>{name} </span>
+            <span>{value} </span>
+            <span>{Math.floor(value / 2)} </span>
+            <span>{Math.floor(value / 5)}</span>
+        </article>
+    );
+
     render() {
-        return <section>{this.props.skills.map(this.renderItem)}</section>;
+        return (
+            <SkillList.Wrapper>
+                {this.props.skills.map(this.renderItem)}
+            </SkillList.Wrapper>
+        );
     }
 
-    renderItem = props => {
+    renderItem = ({ id, base, name } = {}) => {
         const currentValue = getValue(
-            getImprovements(this.context.skills, props.id),
-            props.base
+            getImprovements(this.context.skills, id),
+            base
         );
 
         return (
-            <article>
-                {props.name} {currentValue} | {Math.floor(currentValue / 2)} |{" "}
-                {Math.floor(currentValue / 5)}
-            </article>
+            <SkillList.Item
+                className={`skill-list__item skill-list__item--${id}`}
+                name={name}
+                value={currentValue}
+                key={id}
+            />
         );
     };
 }
-SkillList.contextType = SkillImprovementContext;
